e9a6bbff6e9f9d01b55957c1525cf680c31273a0
Gabriel Soudry <gabriel_s@hotmail.fr>
2022-11-27 22:38:49 +0100

    feat: faceapi functionnal  🎉

--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -13,16 +13,35 @@ function Dashboard() {
 
     React.useEffect(() => {
         const loadModels = async () => {
-            const MODEL_URL = process.env.PUBLIC_URL + '/models';
-            console.log("ici-------------------------")
+            const MODEL_URL = '/models';
+            const mtcnnParams = {
+                // number of scaled versions of the input image passed through the CNN
+                // of the first stage, lower numbers will result in lower inference time,
+                // but will also be less accurate
+                maxNumScales: 10,
+                // scale factor used to calculate the scale steps of the image
+                // pyramid used in stage 1
+                scaleFactor: 0.709,
+                // the score threshold values used to filter the bounding
+                // boxes of stage 1, 2 and 3
+                scoreThresholds: [0.6, 0.7, 0.7],
+                // mininum face size to expect, the higher the faster processing will be,
+                // but smaller faces won't be detected
+                minFaceSize: 50
+            }
+            const options = new faceapi.MtcnnOptions(mtcnnParams)
+            const labels = ['Barney', 'Lily', 'Marshall', 'Robin', 'Ted']
+
             console.log(MODEL_URL)
             Promise.all([
                 faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
                 faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
                 faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
                 faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
-            ]).then(() =>
+                faceapi.nets.mtcnn.loadFromUri(MODEL_URL)
+            ]).then(() => {
                 setModelsLoaded(true)
+            }
             );
         }
         loadModels();
@@ -57,11 +76,11 @@ function Dashboard() {
                 const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
 
                 const resizedDetections = faceapi.resizeResults(detections, displaySize);
-
                 canvasRef && canvasRef.current && (canvasRef.current as any).getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
                 canvasRef && canvasRef.current && faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
                 canvasRef && canvasRef.current && faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
                 canvasRef && canvasRef.current && faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+
             }
         }, 100)
     }
@@ -88,12 +107,15 @@ function Dashboard() {
             </div>
             {
                 captureVideo ?
-                    <div>
-                        <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
-                            <video ref={videoRef} height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} style={{ borderRadius: '10px' }} />
-                            <canvas ref={canvasRef} style={{ position: 'absolute' }} />
+                    modelsLoaded ?
+                        <div>
+                            <div style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}>
+                                <video ref={videoRef} height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay} style={{ borderRadius: '10px' }} />
+                                <canvas ref={canvasRef} style={{ position: 'absolute' }} />
+                            </div>
                         </div>
-                    </div>
+                        :
+                        <div>loading...</div>
                     :
                     <>
                     </>
